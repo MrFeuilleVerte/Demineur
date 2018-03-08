ccca4cc70d507e104758178afa9b021e0b7b64c6
Vincent <vincent.vaucouleur@epitech.eu>
2018-03-08 22:57:29 +0100

    Add Files

---
+++ pushf.sh
@@ -0,0 +1,8 @@
+# @Author: vincent
+# @Date:   2018-03-08T15:47:32+01:00
+# @Filename: push.sh
+# @Last modified by:   vincent
+# @Last modified time: 2018-03-08T19:51:21+01:00
+
+git commit -am "Add Files"
+git push -u origin master
