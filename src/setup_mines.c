1df9d69c0924dd89659004709350d44ba1f36568
Zeagle77 <zeagle7github.com>
2018-03-08 21:54:30 +0100

    Add Files

--- src/setup_mines.c
+++ src/setup_mines.c
@@ -18,33 +18,33 @@ void setup_mines(t_demineur *demineur)
 			tmpCelly = rand()%demineur->mapSize.y;
 		}
 
-		demineur.map[tmpCellx][tmpCelly].isBomb = true;
+		demineur->map[tmpCellx][tmpCelly].isBomb = true;
 
 		//emplacement de la case sur un pave numerique, 5 la bombe
 		
-		if(tmpCellx <= 0 || tmpCelly <= 0)
-		++demineur.map[(tmpCellx - 1)][(tmpCelly - 1)].bombAround; //7
+		if(tmpCellx <= 0 && tmpCelly <= 0)
+		++demineur->map[(tmpCellx - 1)][(tmpCelly - 1)].bombAround; //7
 
 		if(tmpCelly <= 0)
-		++demineur.map[tmpCellx][(tmpCelly - 1)].bombAround; //8
+		++demineur->map[tmpCellx][(tmpCelly - 1)].bombAround; //8
 
-		if(tmpCellx >= demineur.Map_Size.x || tmpCelly <= 0)
-		++demineur.map[(tmpCellx + 1)][(tmpCelly - 1)].bombAround; //9
+		if(tmpCellx >= demineur->mapSize.x && tmpCelly <= 0)
+		++demineur->map[(tmpCellx + 1)][(tmpCelly - 1)].bombAround; //9
 		
 		if(tmpCellx <= 0)
-		++demineur.map[(tmpCellx - 1)][tmpCelly].bombAround; //4
+		++demineur->map[(tmpCellx - 1)][tmpCelly].bombAround; //4
 
-		if(tmpCellx >= demineur.Map_Size.x)
-		++demineur.map[(tmpCellx + 1)][tmpCelly].bombAround; //6
+		if(tmpCellx >= demineur->mapSize.x)
+		++demineur->map[(tmpCellx + 1)][tmpCelly].bombAround; //6
 		
-		if(tmpCellx <= 0 || tmpCelly >= demineur.Map_Size.y)
-		++demineur.map[(tmpCellx - 1)][(tmpCelly + 1)].bombAround; //1
+		if(tmpCellx <= 0 && tmpCelly >= demineur->mapSize.y)
+		++demineur->map[(tmpCellx - 1)][(tmpCelly + 1)].bombAround; //1
 
-		if(tmpCelly >= demineur.Map_Size.y)
-		++demineur.map[tmpCellx][(tmpCelly + 1)].bombAround; //2
+		if(tmpCelly >= demineur->mapSize.y)
+		++demineur->map[tmpCellx][(tmpCelly + 1)].bombAround; //2
 
-		if(tmpCellx >= demineur.Map_Size.x || tmpCelly >= demineur.Map_Size.y)
-		++demineur.map[(tmpCellx + 1)][(tmpCelly + 1)].bombAround; //3
+		if(tmpCellx >= demineur->mapSize.x && tmpCelly >= demineur->mapSize.y)
+		++demineur->map[(tmpCellx + 1)][(tmpCelly + 1)].bombAround; //3
 
 
 	}
